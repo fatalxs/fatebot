3a56655d429e84de30049c8b66caa3c638e9b331
revenantesp <theactualrevenant@gmail.com>
2023-05-13 13:14:12 +0800

    added prefix in config and check for prefix, command handling and ping function

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import discord
 import json
+import time
+
+with open('config.json', "r") as f:
+    config = json.load(f)
+
+prefix = config["prefix"]
+
 
 class myClient(discord.Client):
     async def on_ready(self):
-        print(f"logged on as {self.user}")
+        
+        print(f"logged on as {self.user} (ID: {self.user.id}).")
+        print(f"current prefix is : \"{prefix}\"")
 
     async def on_message(self,msg):
-        print(f"message received from {msg.author}")
-        if msg.author != self.user: # not a bot message
-            await msg.channel.send("hey")
+        if msg.content[0] != prefix:
+            return
+
+        array = msg.content[1:].split(" ")
+        cmd = array[0]
+        args = array[1:]
+
+        if cmd == "ping":
+            await msg.channel.send(f"pong!")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = myClient(intents= intents)
-with open('config.json', "r") as f:
-    config = json.load(f)
+
     
 client.run(config["token"])
